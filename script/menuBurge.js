07e067f2106870e6bde17005a788925318e60481
Jesus Santiago <jesusantiago420@gmail.com>
2023-05-02 18:31:04 -0300

    se termino toda la versión tablet

--- script/menuBurge.js
+++ script/menuBurge.js
@@ -1,20 +1,4 @@
-// let close = document.querySelector(".gg-close-r");
-// let menu = document.querySelector(".menuBurger");
-// let header = document.querySelector(".header");
-// let burger = document.querySelector(".span__burger");
-
-// burger.addEventListener("click", (e)=>{
-//     console.log(e.target)
-//     header.classList.toggle("active")
-//     menu.classList.toggle("menuBurger_none")
-// })
-
-
-// close.addEventListener("click", ()=>{
-//     console.log("burger close")
-//     menu.classList.toggle("menuBurger__none")
-// })
-
+// Menu Burger
 
 const header = document.getElementById("header");
 const burger = document.getElementById("burger");
@@ -24,4 +8,14 @@ burger.addEventListener("click", (e)=>{
     console.log(e.target)
     header.classList.toggle("a");
 
+})
+
+// Menu Filter
+
+const main = document.getElementById("main__Shop");
+const filter = document.getElementById("filter");
+
+
+filter.addEventListener("click", () => {
+    main.classList.toggle("activeFilter")
 })
